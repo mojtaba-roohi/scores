62cf196e403b701557c6741c02f05ecf116e42a4
mojtaba rouhi <roohi.m.1364@gmail.com>
2023-01-07 16:13:25 +0330

    fix bug

--- src/main/java/org/rajman/common/scores/config/RabbitMQConfig.java
+++ src/main/java/org/rajman/common/scores/config/RabbitMQConfig.java
@@ -2,13 +2,14 @@ package org.rajman.common.scores.config;
 
 import org.rajman.common.scores.model.RabbitMQProperties;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @ComponentScan(basePackages = "org.rajman.common.scores.config")
 public class RabbitMQConfig {
 
@@ -19,22 +20,18 @@ public class RabbitMQConfig {
 //        this.cachingConnectionFactory = cachingConnectionFactory;
 //    }
 
-//    @Bean
-//    public ConnectionFactory connectionFactory(RabbitMQProperties rabbitMQProperties) {
-//        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-//        connectionFactory.setUsername(rabbitMQProperties.getUsername());
-//        connectionFactory.setPassword(rabbitMQProperties.getPassword());
-//        connectionFactory.setPort(rabbitMQProperties.getPort());
-//      connectionFactory.setHost(rabbitMQProperties.getHost());
-//        return connectionFactory;
-//    }
+    @Bean
+    public ConnectionFactory connectionF(RabbitMQProperties rabbitMQProperties) {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setUsername(rabbitMQProperties.getUsername());
+        connectionFactory.setPassword(rabbitMQProperties.getPassword());
+        connectionFactory.setPort(rabbitMQProperties.getPort());
+      connectionFactory.setHost(rabbitMQProperties.getHost());
+        return connectionFactory;
+    }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(RabbitMQProperties rabbitMQProperties, CachingConnectionFactory cachingConnectionFactory) {
-        cachingConnectionFactory.setUsername(rabbitMQProperties.getUsername());
-        cachingConnectionFactory.setPassword(rabbitMQProperties.getPassword());
-        cachingConnectionFactory.setPort(rabbitMQProperties.getPort());
-        cachingConnectionFactory.setHost(rabbitMQProperties.getHost());
+    public RabbitTemplate rabbit(ConnectionFactory cachingConnectionFactory) {
         RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory);
         return template;
     }
